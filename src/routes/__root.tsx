3fe7063a78648608b10f131c4bd2dc849822924d
Lucas Volpati <lcs.rodrigues16@hotmail.com>
2025-01-30 14:07:50 -0300

    fix: menu mobile

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,34 +1,76 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
-import { Box, Flex, Heading, Container, Button } from "@chakra-ui/react"
+import { Box, Flex, Heading, Container, Button, IconButton, Stack } from "@chakra-ui/react"
+import { useState } from "react";
+import { LuMenu } from "react-icons/lu"
+
+function RootComponent() {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+    const toggleMenu = () => {
+      setIsMenuOpen(!isMenuOpen);
+    };
+
+    return(
+        <>
+        <Box bg="blue.500" px={4}>
+            <Flex h={16} alignItems="center" justifyContent="space-around">
+                <Heading size="md" color="white" width="60%">
+                    React Project
+                </Heading>
+
+                <IconButton
+                    display={{ base: "flex", md: "none" }}
+                    onClick={toggleMenu}
+                >
+                    <LuMenu />
+                </IconButton>
+
+                <Flex justifyContent={"center"} display={{ base: "none", md: "flex" }} width="40%">
+                    <Button variant="ghost">
+                        <Link px={2} py={8} color="white" to="/" className="[&.active]:font-bold">
+                            Home
+                        </Link> 
+                    </Button>
+                    <Button variant="ghost">
+                        <Link px={2} py={8} color="white" to="/clients" className="[&.active]:font-bold">
+                            Clientes
+                        </Link>
+                    </Button>
+                </Flex>
+
+                {isMenuOpen && (
+                    <Box
+                    position="absolute"
+                    top="60px"
+                    left="0"
+                    right="0"
+                    bg="blue.500"
+                    zIndex="1"
+                    p={4}
+                    shadow="md"
+                  >
+                    <Stack as="nav" spacing={4}>
+                      <Link color="white" to="/">
+                        Home
+                      </Link>
+                      <Link color="white" to="/clients">
+                        Clientes
+                      </Link>
+                    </Stack>
+                  </Box>
+                )}
+            </Flex>
+            </Box>
+        <Container py={6}>
+            <Outlet />
+        </Container>
+        <TanStackRouterDevtools />
+        </>
+    
+    )
+}
 
 export const Route = createRootRoute({
-  component: () => (
-    <>
-      <Box bg="blue.500" px={4}>
-        <Flex h={16} alignItems="center" justifyContent="space-around">
-            <Heading size="md" color="white" width="60%">
-                React Project
-            </Heading>
-
-            <Flex justifyContent={"center"} width="40%">
-                <Button variant="ghost">
-                    <Link px={2} py={8} color="white" to="/" className="[&.active]:font-bold">
-                        Home
-                    </Link> 
-                </Button>
-                <Button variant="ghost">
-                    <Link px={2} py={8} color="white" to="/clients" className="[&.active]:font-bold">
-                        Clientes
-                    </Link>
-                </Button>
-            </Flex>
-        </Flex>
-        </Box>
-      <Container py={6}>
-        <Outlet />
-      </Container>
-      <TanStackRouterDevtools />
-    </>
-  ),
-})
+    component: RootComponent,
+  });
